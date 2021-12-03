98be52bcb90489e0df462ab3f845259e2c6a3fe2
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-12-02 21:50:01 -0300

    add hardhat config with env file and init contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,12 @@
-require("@nomiclabs/hardhat-waffle");
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,6 +14,32 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require("@nomiclabs/hardhat-waffle");
+const env = require('./env.js')
+
 module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      chainId: 1337 //stand
+    },
+    mumbai: {
+      url: env.default.mumbaiUrl,
+      accounts: [env.default.key]
+    },
+    mainnet: {
+      url: env.default.mainnetUrl,
+      accounts: [env.default.key]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        run: 200
+      }
+    }
+  }
 };
