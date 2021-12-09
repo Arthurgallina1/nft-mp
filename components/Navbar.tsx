6573d0f8e8aa17e7466983931e5f9edba9898a44
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-12-09 10:33:31 -0300

    add mynft page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -69,10 +69,10 @@ export default function Navbar() {
               <div className='hidden sm:block sm:ml-6'>
                 <div className='flex space-x-4'>
                   <span className='bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium'>
-                    <Link href='/mint-item'>Mint Tokens</Link>
+                    <Link href='/'>Main Marketplace</Link>
                   </span>
                   <span className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
-                    <Link href='/mint-item'>Main Marketplace</Link>
+                    <Link href='/mint-item'>Mint Tokens</Link>
                   </span>
                   <span className='text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium'>
                     <Link href='/my-nfts'>My NFTs</Link>
